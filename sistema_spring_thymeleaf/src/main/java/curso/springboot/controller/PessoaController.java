c3a5e6e5f711c7a467aca33ddf8a92c4f2e1d8df
Wesley Rodrigues dos Reis <wesleyepolly16@gmail.com>
2022-04-11 13:16:07 -0300

    upload project

---
+++ sistema_spring_thymeleaf/src/main/java/curso/springboot/controller/PessoaController.java
@@ -0,0 +1,178 @@
+package curso.springboot.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+
+import curso.springboot.model.Pessoa;
+import curso.springboot.model.Telefone;
+import curso.springboot.repository.PessoaRepository;
+import curso.springboot.repository.TelefoneRepository;
+
+@Controller
+public class PessoaController {
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	@Autowired
+	private TelefoneRepository telefoneRepository;
+	
+	
+	@RequestMapping(method= RequestMethod.GET, value= "/cadastropessoa")
+	public ModelAndView inicio() {
+		ModelAndView modelView = new ModelAndView("cadastro/cadastropessoa");
+		modelView.addObject("pessoaobj", new Pessoa());
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		modelView.addObject("pessoas", pessoasIt);
+		return modelView;
+	}
+	
+	
+	@RequestMapping(method= RequestMethod.POST, value= "/salvarpessoa")
+	public ModelAndView salvar(@Valid Pessoa pessoa, BindingResult bindingResult) {
+		
+		if(bindingResult.hasErrors()) {
+			ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+			Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+			modelAndView.addObject("pessoas", pessoasIt);
+			modelAndView.addObject("pessoaobj", pessoa);
+			
+			List<String> msg = new ArrayList<String>();
+			for (ObjectError objectError : bindingResult.getAllErrors()) {
+				msg.add(objectError.getDefaultMessage()); //pega msg das anotações @NotEmpty e outras
+			}
+			
+			modelAndView.addObject("msg", msg);
+			
+			return modelAndView;
+		}
+		
+		pessoaRepository.save(pessoa);
+		
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		andView.addObject("pessoas", pessoasIt);
+		andView.addObject("pessoaobj", new Pessoa());
+		
+		return andView;
+	}
+	
+	@RequestMapping(method= RequestMethod.GET, value= "/listapessoas")
+	public ModelAndView pessoas() {
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		andView.addObject("pessoas", pessoasIt);
+		andView.addObject("pessoaobj", new Pessoa());
+		return andView;
+	}
+	
+	@GetMapping("/editarpessoa/{idpessoa}")
+	public ModelAndView editar(@PathVariable("idpessoa") Long idpessoa) {
+		
+		Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
+
+		ModelAndView modelView = new ModelAndView("cadastro/cadastropessoa");
+		modelView.addObject("pessoaobj", pessoa.get());
+		return modelView;
+	}
+	
+	@GetMapping("/removerpessoa/{idpessoa}")
+	public ModelAndView excluir(@PathVariable("idpessoa") Long idpessoa) {
+		
+		pessoaRepository.deleteById(idpessoa);
+
+		ModelAndView modelView = new ModelAndView("cadastro/cadastropessoa");
+		modelView.addObject("pessoas", pessoaRepository.findAll());
+		modelView.addObject("pessoaobj", new Pessoa());
+		return modelView;
+	}
+	
+	@PostMapping("/pesquisarpessoa")
+	public ModelAndView pesquisar(@RequestParam("nomepesquisa") String nomepesquisa) {
+		
+		ModelAndView modelView = new ModelAndView("cadastro/cadastropessoa");
+		modelView.addObject("pessoas", pessoaRepository.findPessoaByName(nomepesquisa));
+		modelView.addObject("pessoaobj", new Pessoa());
+		return modelView;
+	}
+	
+	@GetMapping("/telefones/{idpessoa}")
+	public ModelAndView telefones(@PathVariable("idpessoa") Long idpessoa) {
+		
+		Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
+
+		ModelAndView modelView = new ModelAndView("cadastro/telefones");
+		modelView.addObject("pessoaobj", pessoa.get());
+		modelView.addObject("telefones", telefoneRepository.getTelefones(idpessoa));
+		return modelView;
+	}
+	
+	@PostMapping("/addfonepessoa/{pessoaid}")
+	public ModelAndView addFonePessoa(Telefone telefone, @PathVariable("pessoaid") Long pessoaid) {
+		
+		Pessoa pessoa = pessoaRepository.findById(pessoaid).get();
+		
+		if(telefone != null && telefone.getNumero().isEmpty() || telefone.getTipo().isEmpty()) {
+			
+			ModelAndView modelAndView = new ModelAndView("cadastro/telefones");
+			modelAndView.addObject("pessoaobj", pessoa);
+			modelAndView.addObject("telefones", telefoneRepository.getTelefones(pessoaid));
+			
+			List<String> msg = new ArrayList<String>();
+			
+			if(telefone.getNumero().isEmpty()) {
+				msg.add("Número deve ser informado");
+			}
+			if(telefone.getTipo().isEmpty()) {
+				msg.add("Tipo deve ser informado");
+			}
+			
+			
+			modelAndView.addObject("msg", msg);
+			
+			return modelAndView;
+		}
+		
+		ModelAndView modelAndView = new ModelAndView("cadastro/telefones");
+		
+		telefone.setPessoa(pessoa);
+		telefoneRepository.save(telefone);
+		
+		modelAndView.addObject("pessoaobj", pessoa);
+		modelAndView.addObject("telefones", telefoneRepository.getTelefones(pessoaid));
+		return modelAndView;
+	}
+	
+	
+	@GetMapping("/removertelefone/{idtelefone}")
+	public ModelAndView removertelefone(@PathVariable("idtelefone") Long idtelefone) {
+		
+		Pessoa pessoa = telefoneRepository.findById(idtelefone).get().getPessoa();
+		
+		telefoneRepository.deleteById(idtelefone);
+
+		ModelAndView modelView = new ModelAndView("cadastro/telefones");
+		modelView.addObject("pessoaobj", pessoa);
+		modelView.addObject("telefones", telefoneRepository.getTelefones(pessoa.getId()));
+		return modelView;
+	}
+	
+
+}
---
+++ sistema_spring_thymeleaf/src/main/java/curso/springboot/security/WebConfigSecurity.java
@@ -0,0 +1,55 @@
+package curso.springboot.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableWebSecurity
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private ImplementacaoUserDetailsService implementacaoUserDetailsService;
+		
+	@Override //Configura as solicitações de acesso por Http
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf()
+		.disable() //Desativa as config padrão de memoria 
+		.authorizeRequests() //Permitir restringir acessos
+		.antMatchers(HttpMethod.GET, "/").permitAll() //Qualque uário acessa
+		.anyRequest().authenticated()
+		.and().formLogin().permitAll()// Permite qualquerr usuário
+		.and().logout() //Mapeia url de logout e invalid usuário autenticado
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+	}
+	
+	@Override // Cria autenticação do usuário com o banco de dados ou em memória
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.userDetailsService(implementacaoUserDetailsService)
+		.passwordEncoder(new BCryptPasswordEncoder());
+		
+		/*auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+		.withUser("wesley")
+		.password("$2a$10$5EE5G6G2Uuo4EvgxBH5J5.lD/8bHKSFTUushvJs1waHkTrP5VQgOq") //wrr230808
+		.roles("ADMIN");*/
+	}
+	
+	@Override //Ignora URL especificas
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/materialize/**");
+	}
+	
+	
+}
